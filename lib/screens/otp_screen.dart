a61ccabe3a71e42f7680815d7b20759313a5aaa2
pugal-2004 <lightingradiogamer233@gmail.com>
2024-05-08 14:39:04 +0530

    otp update

--- lib/screens/otp_screen.dart
+++ lib/screens/otp_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:koyambedu/widgets/custom_button.dart';
+import 'package:pinput/pinput.dart';
 
 class OtpScreen extends StatefulWidget {
 
@@ -12,8 +14,89 @@ class OtpScreen extends StatefulWidget {
 }
 
 class _OtpScreenState extends State<OtpScreen> {
+  String? otpCode;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+          child: Column(
+            children: [
+              Align(
+               alignment: Alignment.topLeft,
+               child: GestureDetector(
+                onTap: () => Navigator.of(context).pop(),
+                child: const Icon(Icons.arrow_back),
+               ),
+              ),
+             const  Icon(Icons.image_search,
+              size: 200,
+              ),
+              const SizedBox(height: 10,),
+              const Text("Verification",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const Text("Enter the OTP sended",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20,),
+              Pinput(
+                length: 6,
+                showCursor: true,
+                defaultPinTheme: PinTheme(
+                  width: 60,
+                  height: 60,
+                  
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.purple)
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600
+                  )
+                ),
+                onSubmitted: (value){
+                  setState(() {
+                    otpCode = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 25,),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                child: CustomButton(text: "Verify",
+                 onPressed: (){}
+                 ),
+
+              ),
+
+              const SizedBox(height: 25,),
+              const Text("Didn't recieve code?",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.black
+              ),
+              ),
+              const SizedBox(height: 25,),
+              const Text("Resend Now",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.purple
+              ),
+              ),
+            ],
+            ),
+            ),
+            ),
+            ),
+            );
   }
 }
+
+//verify OTP
+
